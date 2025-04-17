b89c3f2fbcf9de4cae99bd4b2e916431038b5762
Igor <igorfx@mail.ru>
2025-04-18 03:18:56 +0600

    fixes main.py

--- src/processing.py
+++ src/processing.py
@@ -1,22 +1,22 @@
-def filter_by_state(data: list[dict], state: str = 'EXECUTED') -> list:
-    '''Принимает на вход список словарей возвращает новый список
-        словарей, содержащий только те словари, у которых ключ state
-        соответствует указанному значению'''
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list:
+    """Принимает на вход список словарей возвращает новый список
+    словарей, содержащий только те словари, у которых ключ state
+    соответствует указанному значению"""
 
     operations_state = []
 
-    for operation in operations:
-        if operation.get('state', 0) == state:
+    for operation in data:
+        if operation.get("state", 0) == state:
             operations_state.append(operation)
 
     return operations_state
 
 
-def sort_by_date(operations: list[dict], descending: bool = True) -> list:
-    '''Функция принимает список словарей и необязательный параметр,
-        задающий порядок сортировки (по умолчанию — убывание).
-        возвращает новый список, отсортированный по дате'''
+def sort_by_date(data: list[dict], descending: bool = True) -> list:
+    """Функция принимает список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание).
+    возвращает новый список, отсортированный по дате"""
 
-    operations.sort(key=lambda x: x['date'], reverse=descending)
+    data.sort(key=lambda x: x["date"], reverse=descending)
 
-    return operations
+    return data
